c78456073dd45be099fdffebf43b047f88ad924c
MarkAI <2811016860@qq.com>
2025-07-25 21:23:33 +0800

    修复加载动画不显示logo

--- src/components/Branding/ProductLogo/index.tsx
+++ src/components/Branding/ProductLogo/index.tsx
@@ -1,6 +1,8 @@
 import { LobeHub, LobeHubProps } from '@lobehub/ui/brand';
 import { memo } from 'react';
+import { Flexbox } from 'react-layout-kit';
 
+import { BRANDING_NAME } from '@/const/branding';
 import { isCustomBranding } from '@/const/version';
 
 import CustomLogo from './Custom';
@@ -11,6 +13,28 @@ interface ProductLogoProps extends LobeHubProps {
 }
 
 export const ProductLogo = memo<ProductLogoProps>((props) => {
+  const { type, size = 32, ...rest } = props;
+
+  // 如果是 combine 类型，显示 LobeHub logo + MarkAI 文本
+  if (type === 'combine') {
+    return (
+      <Flexbox align={'center'} flex={'none'} horizontal {...rest}>
+        <LobeHub size={size} />
+        <div
+          style={{
+            fontSize: size / 1.5,
+            fontWeight: 'bolder',
+            marginLeft: Math.round(size / 4),
+            userSelect: 'none',
+          }}
+        >
+          {BRANDING_NAME}
+        </div>
+      </Flexbox>
+    );
+  }
+
+  // 其他情况下，如果是自定义品牌使用 CustomLogo，否则使用 LobeHub
   if (isCustomBranding) {
     return <CustomLogo {...props} />;
   }
