c1e4dfee320581cd1e362250e7c2e618b8f6b705
MarkAI <2811016860@qq.com>
2025-08-01 21:43:21 +0800

    back

--- scripts/cdnWorkflow/utils.ts
+++ scripts/cdnWorkflow/utils.ts
@@ -81,7 +81,7 @@ export const fetchImageAsFile = async (url: string, width: number) => {
     const filename = Date.now().toString() + type;
 
     // Step 3: Create a file from the blob
-    const file: File = new File([new Uint8Array(buffer)], filename, {
+    const file: File = new File([buffer], filename, {
       lastModified: Date.now(),
       type: type === '.webp' ? 'image/webp' : blob.type,
     });
