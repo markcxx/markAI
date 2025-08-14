75b1ceee8aa10df22e8fb7c07953205f06cf2fa6
MarkAI <2811016860@qq.com>
2025-08-15 02:57:48 +0800

    feat: 添加Assistant.png图标文件

--- src/const/meta.ts
+++ src/const/meta.ts
@@ -1,8 +1,8 @@
 import { MetaData } from '@/types/meta';
 
-export const DEFAULT_AVATAR = '🤖';
-export const DEFAULT_USER_AVATAR = '🤯';
+export const DEFAULT_AVATAR = '/icons/Assistant.png';
+export const DEFAULT_USER_AVATAR = '/icons/LOGO.png';
 export const DEFAULT_BACKGROUND_COLOR = 'rgba(0,0,0,0)';
 export const DEFAULT_AGENT_META: MetaData = {};
-export const DEFAULT_INBOX_AVATAR = '/icons/Assistant.svg';
+export const DEFAULT_INBOX_AVATAR = '/icons/Assistant.png';
 export const DEFAULT_USER_AVATAR_URL = '/icons/LOGO.png';
